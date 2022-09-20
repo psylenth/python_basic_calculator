4bb1571a10f34b599899dce91b58bb468a3f9419
psylenth <76854340+psylenth@users.noreply.github.com>
2022-09-20 01:55:35 -0400

    Create pycalc.py

---
+++ pycalc.py
@@ -0,0 +1,19 @@
+
+#program to make a simple calculator
+
+
+
+
+#this function adds two numbers
+def add(x, y):
+    return x + y
+
+
+#this function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+#this function factors two numbers
+def multiply(x, y):
+    return x * y
+    
